b321e4d1d3beaad24f62c56fe8455adbfc3cb7fc
Ferdinand Bachmann <theferdi265@gmail.com>
2020-06-17 23:29:37 +0200

    parser: add test for comments

--- src/parser/test.rs
+++ src/parser/test.rs
@@ -397,3 +397,40 @@ fn test_program_inner_newlines() {
         ").is_err()
     );
 }
+
+#[test]
+fn test_program_comments() {
+    assert_eq!(
+        LambdaParser::parse_program(r"
+            # the two possible bools
+            true = a -> b -> a
+            false = a -> b -> b
+
+            # unary bool combinators
+            not = a -> a false true
+        "),
+        Ok(Program(vec![
+            Assignment(Identifier("true"), Application(vec![
+                Expression::Lambda(Lambda(Identifier("a"), Application(vec![
+                    Expression::Lambda(Lambda(Identifier("b"), Application(vec![
+                        Expression::Identifier(Identifier("a"))
+                    ])))
+                ])))
+            ])),
+            Assignment(Identifier("false"), Application(vec![
+                Expression::Lambda(Lambda(Identifier("a"), Application(vec![
+                    Expression::Lambda(Lambda(Identifier("b"), Application(vec![
+                        Expression::Identifier(Identifier("b"))
+                    ])))
+                ])))
+            ])),
+            Assignment(Identifier("not"), Application(vec![
+                Expression::Lambda(Lambda(Identifier("a"), Application(vec![
+                    Expression::Identifier(Identifier("a")),
+                    Expression::Identifier(Identifier("false")),
+                    Expression::Identifier(Identifier("true"))
+                ])))
+            ]))
+        ]))
+    );
+}
